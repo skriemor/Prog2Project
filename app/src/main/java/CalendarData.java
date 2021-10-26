4194f8046493deda00c08d01f6f4ccb2943a5bdd
skriemor <skriemor@gmail.com>
2021-10-26 22:24:11 +0200

    Bugfixek, naptár lépegetés (nem teljes)

---
+++ app/src/main/java/CalendarData.java
@@ -0,0 +1,76 @@
+import android.icu.text.DateFormat;
+import android.icu.text.SimpleDateFormat;
+
+import java.text.ParseException;
+import java.util.Date;
+
+public class CalendarData {
+    DateFormat customDateFormatter = new SimpleDateFormat("yyyy/MM/dd hh:mm");
+    DateFormat customDateParser = new SimpleDateFormat("yyyy.MM.dd.hh.mm");
+    Date startDate, endDate;
+    String location, title, notes;
+
+    public CalendarData(Date startDate, Date endDate, String location, String title, String notes) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.title = title;
+        this.notes = notes;
+    }
+
+    private Date parseRawDate(String dateString){
+        try {
+            return customDateParser.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            Date nulldate = null;
+            return nulldate;
+        }
+
+    }
+
+    private String dateToString(Date d){
+        if (d == null) return "";
+        return customDateFormatter.format(d);
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- app/src/main/java/com/example/prog2project/CalendarFragment.java
+++ app/src/main/java/com/example/prog2project/CalendarFragment.java
@@ -1,25 +1,114 @@
 package com.example.prog2project;
 
+import static android.widget.Toast.makeText;
+
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends Fragment implements CalendarAdapter.OnItemListener {
+
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+    private LocalDate selectedDate;
 
     public CalendarFragment() {
         // Required empty public constructor
     }
-    
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View calFragView = inflater.inflate(R.layout.fragment_calendar, container, false);
+        Button prevMonthButton = (Button) calFragView.findViewById(R.id.prevButton);
+        Button nextMonthButton = (Button) calFragView.findViewById(R.id.nextButton);
+
+        prevMonthButton.setOnClickListener(view -> prevButtonAction());
+        nextMonthButton.setOnClickListener(view -> nextButtonAction());
+
+        return calFragView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        initWidgets();
+        selectedDate = LocalDate.now(); //vegre helyreteve
+        setMonthView();
+    }
+
+    private void setMonthView() {
+
+        monthYearText.setText(monthYearFromDate(selectedDate));
+        ArrayList<String> daysInMonth = daysInMonthArray(selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth,this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(),7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+
+    }
+
+    private ArrayList<String> daysInMonthArray(LocalDate date) {
+        ArrayList<String> daysInMonthArray = new ArrayList<>();
+        YearMonth yearMonth = YearMonth.from(date);
+        int daysInMonth = yearMonth.lengthOfMonth();
+        LocalDate firstOfMonth = selectedDate.withDayOfMonth(1);
+        int dayOfWeek = firstOfMonth.getDayOfWeek().getValue();
+        for (int i = 1; i<=42; i++) {
+            if( i <= dayOfWeek || i > daysInMonth + dayOfWeek){
+                daysInMonthArray.add("");
+            }
+            else{
+                daysInMonthArray.add(String.valueOf(i - dayOfWeek));
+            }
+        }
+        return daysInMonthArray;
+    }
+
+
+    private String monthYearFromDate(LocalDate date){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM YYYY");
+        return date.format(formatter);
+    }
+
+    private void initWidgets() {
+        calendarRecyclerView = getView().findViewById(R.id.CalendarRecycleView);
+        monthYearText = getView().findViewById(R.id.monthText);
+    }
+
+
+    public void prevButtonAction(){
+        selectedDate = selectedDate.minusMonths(1);
+        setMonthView();
+    }
+    public void nextButtonAction(){
+        selectedDate = selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @Override
+    public void onItemClick(int position, String dayText) {
+        if(dayText.equals("")){
+            String message = "Selected date: "+dayText+" "+monthYearFromDate(selectedDate);
+            Toast.makeText(getActivity(),message,Toast.LENGTH_LONG).show();
+
+        }
     }
 }
